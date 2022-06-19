c98e1c67bfffb4f0fe8f28b1acdd83dfd6b35438
nik-mak <nick.makrakis@gmail.com>
2022-06-19 14:03:39 +1000

    update and delete features implemented

--- main.js
+++ main.js
@@ -11,50 +11,31 @@ submitBtn.addEventListener('click', (e) => {
     alert('Item cannot be empty')
   } else {
     inputField.value = ''
-    const newEntry = addNewItem(itemValue)
+    addNewItem(itemValue)
     const listItem = document.createElement('li')
-    listItem.innerHTML = `
-    <input id="${newEntry.description}" type="checkbox"/>
-    <label for="${newEntry.description}" class="tick js-tick">${newEntry.description}</label>
-    <span class="options">
-      <input type="submit" value="Edit" id="edit" />
-      <input type="submit" value="Delete" id="delete" />
-    </span>
-    `
+    listItem.innerHTML = templateHTML(itemValue)
     listElement.append(listItem)
   }
-  // list.push
 })
 
 function addNewItem(value) {
   const entry = {
-    description: value
+    done: false
    }
-   window.localStorage.setItem(`entry${localStorage.length}`, JSON.stringify(entry))
-   list.push(entry)
+   window.localStorage.setItem(value, JSON.stringify(entry))
    return entry
 }
 
 function allStorage(array) {
-      keys = Object.keys(localStorage),
-      i = keys.length
-  while ( i-- ) {
-      array.push( JSON.parse(localStorage.getItem(keys[i])) )
-  }
+  keys = Object.keys(localStorage)
+  list.push(...keys)
 }
 
 function showList(array) {
   listElement.innerHTML = ""
   array.forEach(element => {
     const listItem = document.createElement('li')
-    listItem.innerHTML = `
-    <input id="${element.description}" type="checkbox"/>
-    <label for="${element.description}" class="tick js-tick">${element.description}</label>
-    <span class="options">
-      <input type="submit" value="Edit" id="edit" />
-      <input type="submit" value="Delete" id="delete" />
-    </span>
-    `
+    listItem.innerHTML = templateHTML(element)
     listElement.append(listItem)
   })
 }
@@ -63,25 +44,76 @@ document.addEventListener('click', (e) => {
   if (e.target.matches('input') && e.target.checked === true) {
     let label = e.target.nextElementSibling
     label.classList.add('done')
+    let target = e.target.id
+    let value = JSON.parse(localStorage.getItem(target))
+    value.done = true
+    localStorage.setItem(target, JSON.stringify(value))
   } else if (e.target.matches('input') && e.target.checked === false) {
-    label = e.target.nextElementSibling
+    let label = e.target.nextElementSibling
     label.classList.remove('done')
+    let target = e.target.id
+    let value = JSON.parse(localStorage.getItem(target))
+    value.done = false
+    localStorage.setItem(target, JSON.stringify(value))
   }
 })
 
-// document.addEventListener('click', (e) => {
-//   if (e.target.value === 'Edit') {
+function isDone(array) {
+  array.forEach((element) => {
+    let a = JSON.parse(localStorage.getItem(element))
+    if (a.done === true) {
+      let b = document.querySelector(`#${element}`)
+      b.checked = true
+      b.nextElementSibling.classList.add('done')
+    }
+  })
+}
 
-//   }
-// })
+document.addEventListener('click', (e) => {
+  if (e.target.value === 'Delete') {
+    const a = e.target.parentElement
+    const b = a.parentElement
+    const c = b.firstElementChild.id
+    localStorage.removeItem(c)
+    b.remove()
+  } else if (e.target.value === 'Edit') {
+    const a = e.target.parentElement.parentElement
+    const b = a.firstElementChild.id
+    a.innerHTML = `
+    <div class="update-item">
+      <input id="${b}" type="text" placeholder="${b}" />
+      <input type="submit" value="Update" id="update" />
+    </div>
+    `
+    const updateBtn = document.querySelector('#update')
+    updateBtn.addEventListener('click', event => {
+      const c = document.querySelector(`#${b}`)
+      if (c.value === '') {
+        a.innerHTML = templateHTML(b)
+      } else {
+        a.innerHTML = templateHTML(c.value)
+        const updatedEntry = {
+          done: false
+        }
+        window.localStorage.setItem(c.value, JSON.stringify(updatedEntry))
+        localStorage.removeItem(b)
+      }
+    })
 
-// document.addEventListener('click', (e) => {
-//   if (e.target.value === 'Delete') {
-//     let a = e.target.parentElement
-//     let b = a.parentElement
-//     b.remove()
-//   }
-// })
+  }
+})
+
+function templateHTML(value) {
+  return `
+    <input id="${value}" type="checkbox"/>
+    <label for="${value}" class="tick js-tick">${value}</label>
+    <span class="options">
+      <input type="submit" value="Edit" id="edit" />
+      <input type="submit" value="Delete" id="delete" />
+    </span>
+  `
+}
 
 allStorage(list)
 showList(list)
+isDone(list)
